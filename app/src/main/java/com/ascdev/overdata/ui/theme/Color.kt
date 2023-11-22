f5af8da015429fb9a543b4bb72192859475cefc1
André Salazar <71437593+AndreSC1218@users.noreply.github.com>
2023-11-22 18:01:44 +0100

    chore: initial project setup
    Added the base project structure with initial files and folders. Configured dependencies and set up the development environment

---
+++ app/src/main/java/com/ascdev/overdata/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.ascdev.overdata.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF3A486D)
+val Background = Color(0xFF96A2BA)
+val OverDataOrange = Color(0xFFFF9100)
+val OverDataGray = Color(0xFFE3E7F0)
